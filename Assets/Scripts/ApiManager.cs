35f05d30982170439afd0ce1a9baac4cab891274
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2023-02-19 22:35:56 -0500

    Terminado

--- Assets/Scripts/ApiManager.cs
+++ Assets/Scripts/ApiManager.cs
@@ -16,8 +16,10 @@ public class ApiManager : MonoBehaviour
     public int[] deck = { 1, 32, 358, 578, 601 };
 
     [SerializeField] private TMP_InputField field;
-    private string api = "https://rickandmortyapi.com/api/character/";
+    [SerializeField] private TMP_Text errorMessage, userName;
+    private string api = "https://rickandmortyapi.com/api/character/", server = "https://my-json-server.typicode.com/Menguiza/Sistemas/users/";
     private int index = 0;
+    private string id;
 
     private void Awake()
     {
@@ -25,8 +27,11 @@ public class ApiManager : MonoBehaviour
         manager = this;
     }
 
+    #region CardRelated
+
     public void Request()
     {
+        errorMessage.text = "";
         characters.Clear();
         GetCharactersMethod(deck);
     }
@@ -41,14 +46,17 @@ public class ApiManager : MonoBehaviour
         StartCoroutine(DownloadImage(characters.ElementAt(index).image, img));
     }
     
-    IEnumerator GetCharacter(int[] cards) 
+    IEnumerator GetCharacter(int[] cards)
     {
+        errorMessage.text = "";
+        
         UnityWebRequest www = UnityWebRequest.Get(api + cards[index]);
         yield return www.SendWebRequest();
  
         if(www.result != UnityWebRequest.Result.Success) 
         {
             Debug.Log("Connection Error: " + www.error);
+            errorMessage.text = "Connection Error: " + www.error;
         }
         else 
         {
@@ -64,7 +72,7 @@ public class ApiManager : MonoBehaviour
                 string message = "Status: " + www.responseCode;
                 message += "\nContent-type: " + www.GetResponseHeader("content-type");
                 message += "\nError: " + www.error;
-                Debug.Log(message);
+                errorMessage.text = message;
             }
         }
 
@@ -83,6 +91,8 @@ public class ApiManager : MonoBehaviour
     
     IEnumerator DownloadImage(string url, RawImage targetTxt)
     {
+        errorMessage.text = "";
+        
         UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
         yield return request.SendWebRequest();
 
@@ -95,6 +105,59 @@ public class ApiManager : MonoBehaviour
             targetTxt.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
         }
     }
+
+    #endregion
+
+    #region UserRelated
+
+    public void LoadUser()
+    {
+        if(!string.IsNullOrEmpty(field.text) && int.TryParse(id, out int numericValue)) StartCoroutine(GetUser(int.Parse(id)));
+        else if (string.IsNullOrEmpty(field.text)) errorMessage.text = "Type an user id to search";
+        else if (!int.TryParse(id, out int numericValue1)) errorMessage.text = "Using wrong format";
+    }
+
+    public void IdToCheck()
+    {
+        id = field.text;
+    }
+    
+    IEnumerator GetUser(int id)
+    {
+        errorMessage.text = "";
+        
+        UnityWebRequest www = UnityWebRequest.Get(server + id);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Connection Error: " + www.error);
+            errorMessage.text = "Connection Error: " + www.error;
+        }
+        else
+        {
+            if (www.responseCode == 200)
+            {
+                string responseText = www.downloadHandler.text;
+
+                User user = JsonUtility.FromJson<User>(responseText);
+
+                deck = user.deck;
+                userName.text = user.name;
+                
+                Request();
+            }
+            else
+            {
+                string message = "Status: " + www.responseCode;
+                message += "\nContent-type: " + www.GetResponseHeader("content-type");
+                message += "\nError: " + www.error;
+                errorMessage.text = message;
+            }
+        }
+    }
+
+    #endregion
 }
 
 [System.Serializable]
@@ -104,4 +167,12 @@ public class Character
     public string name;
     public string species;
     public string image;
+}
+
+[System.Serializable]
+public class User
+{
+    public int id;
+    public string name;
+    public int[] deck;
 }
